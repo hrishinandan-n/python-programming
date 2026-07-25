232ef0f8731dbff636b5f904aa93a0616fef0642
hrishinandan-n <hrishinandan.dev@gmail.com>
2026-07-25 21:46:51 +0530

    Added some basic things

---
+++ python-for-ai/01 basic.py
@@ -0,0 +1,25 @@
+print("Hello, World!")
+
+# Variables
+name = "Alice"
+age = 25
+# is_Loggedin variable:introduces boolean datatype
+is_Loggedin = True
+
+print(name, age, is_Loggedin)
+
+# Comments
+'''
+This is a multi-line comment
+that helps for better documentation
+of the codebase.
+'''
+
+# Data Types
+integer = 5
+decimal = 5.5
+charachter = 'a'
+word = "abcd"
+# Calculating length of a string
+len(word)
+
